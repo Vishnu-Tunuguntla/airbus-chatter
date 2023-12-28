fdd4a30e75afc9446ef1a8d80c1b9ac77a420bb8
vishnut04 <vtunuguntla022@gmail.com>
2023-12-28 16:09:33 -0500

    oops

--- db_init.py
+++ db_init.py
@@ -1,1 +1,13 @@
-print("testing")
+import psycopg2
+
+
+conn = psycopg2.connect(
+    dbname="airbus-database", 
+    user="T1SSU3", 
+    password="xu24DYAgldOaP0TcyLGG", 
+    host="airbus-database.c9gssgcyk1lb.us-east-1.rds.amazonaws.com", 
+    port="5432"
+)
+
+cursor = conn.cursor()
+
